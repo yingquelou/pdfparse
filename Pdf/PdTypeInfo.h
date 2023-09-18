7ba75c5b433d1a5b0810dba5f21ca43f8ed30cce
yingquelou <2695193723@qq.com>
2023-09-18 22:16:53 +0800

    rebuild

---
+++ Pdf/PdTypeInfo.h
@@ -0,0 +1,34 @@
+#ifndef PDTYPEINFO_H
+#define PDTYPEINFO_H 1
+typedef enum pdTypeInfo
+{
+    /* 操作数 */
+    // 简单类型
+
+    pdTypeBoolean,
+    pdTypeInteger,
+    pdTypeReal,
+    pdTypeString,
+    pdTypeName,
+    // 复合类型
+
+    pdTypeArray,
+    pdTypeDictionary,
+    pdTypeStream,
+    pdTypeObjsXrefNum,
+
+    /* 操作符 */
+    pdOperateLA,
+    pdOperateRA,
+    pdOperateLD,
+    pdOperateRD,
+} PdTypeInfo;
+// 非pdf类型,旨在引用pdf类型对象
+typedef struct pdObj
+{
+    // 指向任意pdf类型对象
+    void *obj;
+    // 该对象的类型信息
+    PdTypeInfo typeInfo;
+} pdObj, *PdObj;
+#endif
--- Pdf/Pdf.h
+++
@@ -1,4 +0,0 @@
-#ifndef PDF_H
-#define PDF_H 1
-#include "PdXref.h"
-#endif
