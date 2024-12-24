0a2b41c44c434e75fb50ffa82322a230d1c1d514
yingquelou <1720042096@qq.com>
2024-12-24 22:51:50 +0800

    use jsoncpp?

--- Pdf/src/yacc/main.cpp
+++ Pdf/src/yacc/main.cpp
@@ -4,13 +4,18 @@ int main(int argc, char const *argv[])
 {
   for (size_t i = 1; argv[i]; i++)
   {
-    FILE *f = freopen(argv[1], "rb", stdin);
+    FILE *f = fopen(argv[i], "rb");
     if (f)
     {
+      std::string log(argv[i]);
+      log += ".log";
+      FILE *fe = freopen(log.c_str(), "wb", stderr);
+      yyrestart(f);
       yy::parser parser;
-      // parser.set_debug_level(1);
+      parser.set_debug_level(1);
       parser();
       fclose(f);
+      fclose(fe);
     }
   }
   return 0;
