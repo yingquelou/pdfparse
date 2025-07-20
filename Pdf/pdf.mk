b3f6d2b435b3f719a6195d8ec0eeb935409f7288
yingquelou <1720042096@qq.com>
2025-07-21 06:38:26 +0800

    Refactor build system and remove unused files
    - Updated CMakeLists.txt to use a new project variable and changed the target link library.
    - Removed obsolete build.sh script that was responsible for generating lexer and parser files.
    - Deleted unused utility functions in cmake/utils.cmake.
    - Introduced router.cmake to manage subdirectories and files more effectively.
    - Removed start.bat script which was used for building the project.
    - Deleted test.cpp as it contained no functional code.

--- Pdf/pdf.mk
+++
@@ -1,8 +0,0 @@
-LEXPATH=pdf.lex
-YACCPATH=pdf.y
-PREFIX=pdf
-YACCHEADPATH="$(PREFIX).tab.h"
-LEXHEADPATH=lex.$(PREFIX).h
-all:
-	$(LEX) -L -i --prefix=$(PREFIX) -DYACCHEADPATH='$(YACCHEADPATH)' --header-file=$(LEXHEADPATH) $(LEXPATH)
-	$(YACC) --define=LEXHEADPATH=$(LEXHEADPATH) --warnings=deprecated --defines -l --name-prefix=$(PREFIX) $(YACCPATH)
